fa3aa8d5ee6c85bde15aaaa31ce6c4bf6c253454
diego <dondiegorada@gmail.com>
2023-12-15 00:01:45 +0000

    Fix: Se soluciona conflicto

--- class/db.class.php
+++ class/db.class.php
@@ -6,7 +6,7 @@
     private $passwd;
     private $dbname;
 
-    function __construct($host='localhost', $username='u172854659_jhonfgates', $passwd='a+7Rv~YN/', $dbname='u172854659_opengates') {
+    function __construct($host='172.18.0.2', $username='johndev', $passwd='ninguna', $dbname='oei') {
       $this->host = $host;
       $this->username = $username;
       $this->passwd = $passwd;
